32cbfb87ecef4f7cc90018aa6160b9db2226c4d0
notrickyyy <rikeshbaniyaa@gmail.com>
2023-01-22 23:54:07 +0545

    imgbox ma image host gariyo

--- app/src/main/java/com/rikesh/cinemaghar/utils/Constants.java
+++ app/src/main/java/com/rikesh/cinemaghar/utils/Constants.java
@@ -6,5 +6,5 @@ public class Constants {
 
     //movie tanney api banauna cha aba
     public static String BASE_URL_SEARCH = "https://api.jsonbin.io/v3/b/63c8019c01a72b59f24e0867";
-    public static String IMAGE_BASE_URL = "https://assets.voxcinemas.com/posters/";
+    public static String IMAGE_BASE_URL = "https://images2.imgbox.com/";
 }

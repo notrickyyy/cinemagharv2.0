6eb6acbaf36de1f8309f7e261efee848201d6c69
notrickyyy <rikeshbaniyaa@gmail.com>
2023-01-23 22:29:45 +0545

    movie ra trailer added

--- app/src/main/java/com/rikesh/cinemaghar/utils/Constants.java
+++ app/src/main/java/com/rikesh/cinemaghar/utils/Constants.java
@@ -1,8 +1,11 @@
 package com.rikesh.cinemaghar.utils;
 
 public class Constants {
-    public static String BASE_URL_POPULAR = "https://api.jsonbin.io/v3/b/63c8019c01a72b59f24e0867?meta=false";
-    public static String BASE_URL_NOW_PLAYING = "https://api.jsonbin.io/v3/b/63c8019c01a72b59f24e0867?meta=false";
+    //Movie haru haley
+    public static String BASE_URL_POPULAR = "https://api.jsonbin.io/v3/b/63ceb8deace6f33a22c62686?meta=false";
+
+    //Trailer haru haley
+    public static String BASE_URL_NOW_PLAYING = "https://api.jsonbin.io/v3/b/63ceb94aace6f33a22c626e1?meta=false";
 
     //movie tanney api banauna cha aba
     public static String BASE_URL_SEARCH = "https://api.jsonbin.io/v3/b/63c8019c01a72b59f24e0867";
